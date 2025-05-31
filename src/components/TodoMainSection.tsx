54db9265b85acf0fa20ec8fd3fd1a0f6bae14b5a
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-05-30 19:46:17 -0700

    added animation with framerMotion for enter and exit todo.

--- src/components/TodoMainSection.tsx
+++ src/components/TodoMainSection.tsx
@@ -1,6 +1,7 @@
 import { IoMdClose } from "react-icons/io";
 import { useAppDispatch, useAppSelector } from "../types/reduxHooksType";
 import { removeTodo } from "../features/todos/todosSlice";
+import { AnimatePresence, motion } from "framer-motion";
 
 const TodoMainSection = () => {
   const todos = useAppSelector((state) => state.todos);
@@ -12,41 +13,45 @@ const TodoMainSection = () => {
 
   return (
     <ul className="divide-y divide-gray-100 dark:divide-gray-700">
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          className={`todo-item animate-fade-in ${
-            todo.completed ? "completed" : ""
-          }`}
-        >
-          <input
-            type="checkbox"
-            className="checkbox"
-            checked={todo.completed}
-          />
-          <span
-            className={`todo-text flex-1 transition-all duration-300 ${
-              todo.completed
-                ? "text-green-600 dark:text-green-400 line-through opacity-70"
-                : "text-yellow-500 dark:text-yellow-400"
-            }`}
+      <AnimatePresence>
+        {todos.map((todo) => (
+          <motion.li
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.3 }}
+            key={todo.id}
+            className={`todo-item ${todo.completed ? "completed" : ""}`}
           >
-            {todo.text}
-          </span>
-          <button
-            className="btn btn-danger p-2"
-            onClick={() => handleDeleteTodo(todo.id)}
-            aria-label="Delete todo"
-          >
-            <IoMdClose className="text-white text-xl" />
-          </button>
-        </li>
-      ))}
-      {todos.length === 0 && (
-        <li className="text-center py-8 text-gray-500 dark:text-gray-400 animate-fade-in">
-          No tasks yet. Add one above!
-        </li>
-      )}
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={todo.completed}
+            />
+            <span
+              className={`todo-text flex-1 transition-all duration-300 ${
+                todo.completed
+                  ? "text-green-600 dark:text-green-400 line-through opacity-70"
+                  : "text-yellow-500 dark:text-yellow-400"
+              }`}
+            >
+              {todo.text}
+            </span>
+            <button
+              className="btn btn-danger p-2"
+              onClick={() => handleDeleteTodo(todo.id)}
+              aria-label="Delete todo"
+            >
+              <IoMdClose className="text-white text-xl" />
+            </button>
+          </motion.li>
+        ))}
+        {todos.length === 0 && (
+          <li className="text-center py-8 text-gray-500 dark:text-gray-400 animate-fade-in">
+            No tasks yet. Add one above!
+          </li>
+        )}
+      </AnimatePresence>
     </ul>
   );
 };
